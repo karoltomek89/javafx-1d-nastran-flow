412d39ec8bfaee19495092bb3e49aa8da28db635
karoltomek <karoltomek@o2.pl>
2021-07-05 07:57:14 +0200

    updated data for mediums (air and water at 20C, normal pressure)

--- src/main/java/com/kt/javafx/oneDNastranFlow/model/Medium.java
+++ src/main/java/com/kt/javafx/oneDNastranFlow/model/Medium.java
@@ -1,8 +1,10 @@
 package com.kt.javafx.oneDNastranFlow.model;
 
 public enum Medium {
-    AIR_20C_T_MM_S("0.65","4.2e9","1e-9","1.0E-9"),
-    WATER_20C_T_MM_S("0.65","4.2e9","1e-9","1.0E-9");
+    AIR_20C_T_MM_S("0.025","1e9","1e-12","1.82e-11"),
+    WATER_20C_T_MM_S("0.6","4.2e9","1e-9","1e-9"),
+    AIR_20C_KG_M_S("0.025","1000","1.2","1.82E-5"),
+    WATER_20C_KG_M_S("0.6","4200","1000","0.001");
 
     private String conductivity;
     private String heatCapacity;
